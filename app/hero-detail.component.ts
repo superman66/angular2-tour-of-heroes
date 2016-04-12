6481b9dd22ccbc793b9746d1819cceb0968f1ad0
superman <chc1006@163.com>
2016-04-12 18:50:00 +0800

    init

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by superman on 2016/4/12.
+ */
+import {Component, Input} from 'angular2/core';
+
+@Component({
+    selector: 'my-hero-detail',
+})
+
+export class HeroDetailComponent {}
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by superman on 2016/4/12.
+ */
+import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent);
