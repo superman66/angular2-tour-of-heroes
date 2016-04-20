17c1811eee7a3ca27e0c1a98e38bf31a80660c2c
superman <chc1006@163.com>
2016-04-20 18:58:44 +0800

    learn Services

---
+++ app/dashboard.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by superman on 2016/4/20.
+ */
+import {Component} from "angular2/core";
+@Component({
+    selector: 'my-dashboard',
+    template: '<h3>My Dashboard</h3>'
+})
+
+export class DashboardComponent{
+    
+}
